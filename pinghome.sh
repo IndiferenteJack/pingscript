85556ba8d3bffbec3da69d0fd316a2230acfb7bb
IndiferenteJack <david99t@hotmail.com>
2022-02-20 03:32:42 +0100

    Update pinghome.sh

--- pinghome.sh
+++ pinghome.sh
@@ -16,38 +16,44 @@ infomessage2= %e2%9c%85 Connection back up! %0A$(date +"%a, %d. %B %Y, %H:%M:%S"
 infomessage3= %f0%9f%9a%a8 Connection timeout! %0A$(date +"%a, %d. %B %Y, %H:%M:%S")
 
 # START ACTION DO NOT CHANGE #
-timeout=0
+echo $(date) 'Starting monitoring'
+curl -s --max-time 10 -X POST ${botapi} --data "chat_id=${groupId1}" --data "disable_web_page_preview=true" --data "text=${infomessage1}" --connect-timeout 30 > /dev/null
 
-curl -s --max-time 10 -X POST ${botapi} --data "chat_id=${groupId1}" --data "disable_web_page_preview=true" --data "text=infomessage1" --connect-timeout 30 > /dev/null
-
+if [ ! -f /var/log/pinghome-log.txt ] || grep -Fxq "" /var/log/pinghome-log.txt; 
+then
+    echo "offline" > /var/log/pinghome-log.txt;
+fi
 sleep 2
 
+# ENTER LOOP
 while :
 do
 
 ping -c 4 $server > /dev/null
 status=$?
+if [ $status -eq 0 ] 
+  then 
+    status="up" 
+  else 
+    status="down"
+fi
 
-if [ $status -eq 0 ] && grep -Fxq "offline" pinghome-log.txt
-then 
-   if [ $timeout -eq 0 ]
-        then
-        curl -s --max-time 10 -X POST ${botapi} --data "chat_id=${groupId1}" --data "disable_web_page_preview=true" --data "text=infomessage2" --connect-timeout 30 > /dev/null
-        timeout=1
-	echo "online" > pinghome.txt
-   fi
-
-elif [ $status -ne 0 ] && grep -Fxq "online" pinghome.txt
+if [ $status = up ] && grep -Fxq "offline" /var/log/pinghome-log.txt
 then
-   if [ $timeout -eq 1 ] 
-        then
-        curl -s --max-time 10 -X POST ${botapi} --data "chat_id=${groupId1}" --data "disable_web_page_preview=true" --data "text=infomessage3" --connect-timeout 30 > /dev/null
-        timeout=0
-	echo "offline" > pinghome.txt
-   fi
+     echo $(date) 'Internet is back up.'
+     curl -s --max-time 10 -X POST ${botapi} --data "chat_id=${groupId1}" --data "disable_web_page_preview=true" --data "text=${infomessage2}" --connect-timeout 30 > /dev/null
+     echo "online" > /var/log/pinghome-log.txt
+
+elif [ $status = down ] && grep -Fxq "online" /var/log/pinghome-log.txt
+then
+    echo $(date) 'Internet timeout.'
+    curl -s --max-time 10 -X POST ${botapi} --data "chat_id=${groupId1}" --data "disable_web_page_preview=true" --data "text=${infomessage3}" --connect-timeout 30 > /dev/null
+    echo "offline" > /var/log/pinghome-log.txt
+    
 fi
 
 sleep $interval
+#echo 'Sleep time ' ${interval}
 
 done
 
